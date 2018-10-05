e19439eb798ec07f0ad4db905077ebb2b450dcb0
Aarkan1 <37023580+Aarkan1@users.noreply.github.com>
2018-10-05 18:40:15 +0200

    Första dagens version
    Ska lägga till kommentarer

--- Project Robotgame/src/com/game/Gameboard.java
+++ Project Robotgame/src/com/game/Gameboard.java
@@ -10,6 +10,10 @@ public class Gameboard {
     String cheetahTile = "C|";
     String zebraTile = "Z|";
 
+    public int getGAME_SIZE(){
+        return GAME_SIZE - 1;
+    }
+
     public void gameboard() {
 
         for (int i = 0; i < gameboard.length; i++) {
@@ -19,29 +23,33 @@ public class Gameboard {
         }
     }
 
-    public void spawnRobot(){
+    public void spawnRobot(Robot z, Robot c){
 
-        ZebraRobot z = new ZebraRobot();
 
-        CheetahRobot c = new CheetahRobot();
+            gameboard[z.getCoordX()][z.getCoordY()] = defaultTile;
+            z.doRun();
+            gameboard[z.getCoordX()][z.getCoordY()] = zebraTile;
 
-        gameboard[c.getCoordX()][c.getCoordY()] = cheetahTile;
-
-        gameboard[z.getCoordX()][z.getCoordY()] = zebraTile;
+            gameboard[c.getCoordX()][c.getCoordY()] = defaultTile;
+            c.doRun();
+            gameboard[c.getCoordX()][c.getCoordY()] = cheetahTile;
 
 
     }
 
 
+
     public void printBoard() {
 
-        System.out.println();
+
         for (int i = 0; i < gameboard.length; i++) {
+            System.out.print("|");
             for (int j = 0; j < gameboard.length; j++) {
                 System.out.printf("%s", gameboard[i][j]);
             }
             System.out.println();
         }
+        System.out.println();
     }
 
 
