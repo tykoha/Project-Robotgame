a05acbf3eeb2b8a3b178615e5867ed6bc759d4bc
Aarkan1 <37023580+Aarkan1@users.noreply.github.com>
2018-10-05 18:39:05 +0200

    Första dagens version
    Ska lägga till kommentarer

--- Project Robotgame/src/com/game/Game.java
+++ Project Robotgame/src/com/game/Game.java
@@ -1,7 +1,41 @@
 package com.game;
 
+import java.util.ArrayList;
+
 public class Game {
 
+    public void gameloop() {
 
+        Gameboard game = new Gameboard();
+        game.gameboard();
+
+        ZebraRobot zebra = new ZebraRobot();
+        CheetahRobot cheetah = new CheetahRobot();
+
+        ArrayList<Robot> zebras = new ArrayList<>();
+        ArrayList<Robot> cheetahs = new ArrayList<>();
+
+        zebras.add(zebra);
+        cheetahs.add(cheetah);
+
+
+
+        while(!zebras.isEmpty()){
+
+            game.spawnRobot(zebras.get(0),cheetahs.get(0));
+
+            game.printBoard();
+
+
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+
+            }
+
+        }
+
+
+    }
 
 }
